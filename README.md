54d805fb418dc50d7bd23aa2c4560514a515f29e
steime <dominiksteinmann@bluewin.ch>
2023-02-16 10:32:18 +0100

    finish the tutorial animation and refactor the code to the project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# greensock_tutorial
+# greensock tutorial
+
+You can run this tutorial by runing the index.html using the live server plugin
--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-import gsap from "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"
-
-const element = document.getElementById('.box');
-gsap.to(element, { x: 500, duration: 2 });
-gsap.to(element, { x: 500, duration: 2, ease: 'ease-out' });
-gsap.to(element, { x: 500, duration: 2 })
-    .to(element, { y: 200, duration: 1 });
+const element = "#box"
+gsap.to(element, { x: 500, duration: 2, ease: 'ease-out', rotation: 360})
+gsap.to(element, { y: 200, duration: 1 });
